af8ebc43f8cb518ccd3aaa66260db08dbab5cd55
Flors Rueda <ruedaflors@gmail.com>
2024-05-20 17:58:48 +0200

    add loop to menu

--- app/Program.cs
+++ app/Program.cs
@@ -1,11 +1,50 @@
 ﻿Console.WriteLine("Hello!");
-Console.WriteLine("[S]ee all TODOs");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
 
-string userInput = Console.ReadLine();
+bool isRunning = true;
 
-Console.WriteLine(userInput);
+while (isRunning)
+{
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+    var userInput = Console.ReadLine();
+
+    switch (userInput)
+    {
+        case "E":
+        case "e":
+            isRunning = false;
+            break;
+        case "S":
+        case "s":
+            Console.WriteLine("See all");
+            break;
+        case "A":
+        case "a":
+            Console.WriteLine("Add to do");
+            break;
+        case "R":
+        case "r":
+            Console.WriteLine("Remove to do");
+            break;
+        default:
+            Console.WriteLine("Invalid choice.");
+            break;
+    }
+
+    Console.WriteLine("");
+    Console.WriteLine("");
+    if (userInput == "e" || userInput == "E")
+    {
+        Console.WriteLine("Bye!");
+    }
+    else
+    {
+        Console.WriteLine("Something else?");
+    }
+}
+
+
 
 
