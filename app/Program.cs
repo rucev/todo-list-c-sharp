5b2525efa13d320a3396136aea7183674e9327b5
Flors Rueda <ruedaflors@gmail.com>
2024-05-20 12:06:33 +0200

    initial commit

---
+++ app/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = Console.ReadLine();
+
+Console.WriteLine(userInput);
+
+
